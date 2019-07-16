9e29507acc1a6e9c96eccac12dfb15f1556c8015
liyongxing86 <18209262612@163.com>
2019-07-16 17:02:02 +0800

    test

---
+++ src/main/java/com/qf/lyx/Day0716DemoApplication.java
@@ -0,0 +1,13 @@
+package com.qf.lyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day0716DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day0716DemoApplication.class, args);
+	}
+
+}
